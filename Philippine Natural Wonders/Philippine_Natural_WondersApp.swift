7109541037dfb561b3098d3733e439559dd09be4
alexmortelsison <alexmortelsison@gmail.com>
2024-03-31 12:10:26 +0900

    Initial Commit

---
+++ Philippine Natural Wonders/Philippine_Natural_WondersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Philippine_Natural_WondersApp.swift
+//  Philippine Natural Wonders
+//
+//  Created by Alex Sison on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Philippine_Natural_WondersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
